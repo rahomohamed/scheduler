8713d98d0fbe0f38033b2b09bfdd79235a00f049
Raho <rahojmohamed@gmail.com>
2020-03-23 00:11:18 -0400

    implemented custom hook with tests passing

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,8 +4,25 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition() { /* ... */ }
-  function back() { /* ... */ }
+  const transition = function(mode, replace = false) {
+  
+    if (replace) {
+      setMode(mode);
+    } else {
+  
+      setMode(mode);
+      setHistory([...history, mode]);
+    }
+  };
+  const back = function() {
+  
+  const hist = [...history].slice(0, history.length-1);
+
+    if (hist.length >= 1) {
+    setHistory(hist);
+    setMode(hist[hist.length - 1]);
+  }
+};
 
   return { mode, transition, back };
 };
